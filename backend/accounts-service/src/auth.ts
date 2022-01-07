81f284622ec63a80e5cf82fb8e73c84fdf66cd1d
André Quintino <pandrequintino@gmail.com>
2022-01-07 23:59:55 +0000

    M03: L03: 02 – JWT Sign e Verify

--- backend/accounts-service/src/auth.ts
+++ backend/accounts-service/src/auth.ts
@@ -1,4 +1,13 @@
 import bcrypt from 'bcryptjs';
+import fs from 'fs';
+import jwt, { VerifyOptions } from 'jsonwebtoken';
+
+const privateKey = fs.readFileSync('./keys/private.key', 'utf8');
+const publicKey = fs.readFileSync('./keys/public.key', 'utf8');
+
+const { JWT_EXPIRES } = process.env;
+
+const JWT_ALGORITHM = 'RS256';
 
 function hashPassword(password: string) {
 	return bcrypt.hashSync(password, 10);
@@ -8,4 +17,29 @@ function comparePassword(password: string, hashPassword: string) {
 	return bcrypt.compareSync(password, hashPassword);
 }
 
-export { hashPassword, comparePassword };
+type Token = { accountId: number };
+
+function sign(accountId: number) {
+	const token: Token = { accountId };
+
+	return jwt.sign(token, privateKey, {
+		expiresIn: parseInt(JWT_EXPIRES),
+		algorithm: JWT_ALGORITHM,
+	});
+}
+
+async function verify(token: string) {
+	try {
+		const tokenDecoded: Token = (await jwt.verify(token, publicKey, {
+			algorithm: [JWT_ALGORITHM],
+		} as VerifyOptions)) as Token;
+
+		return { accountId: tokenDecoded.accountId };
+	} catch (error) {
+		console.log(`verify: ${error}`);
+
+		return null;
+	}
+}
+
+export { hashPassword, comparePassword, sign, verify };
